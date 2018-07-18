359a260ddba5d29fdbc22b154897dd45c0797c3f
Raissa North <raissa@silverstripe.com>
2018-07-19 09:40:19 +1200

    NEW Dropdown atop history viewer for holding actions
    In order to allow a user to enter or exit compare mode there is a button
    on the heading row that will reveal more actions when clicked. At
    current the only action present is a toggle to enter or exit compare
    mode, and reflect the state (check means compare mode is active).

--- client/src/components/HistoryViewer/HistoryViewerHeading.js
+++ client/src/components/HistoryViewer/HistoryViewerHeading.js
@@ -1,21 +1,103 @@
 import i18n from 'i18n';
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { setCompareMode } from 'state/historyviewer/HistoryViewerActions';
+import { compose } from "redux";
+import { connect } from "react-redux";
 
-const HistoryViewerHeading = (props) => (
-  <tr>
-    <th>#</th>
-    <th>{i18n._t('HistoryViewer.Record', 'Record')}</th>
-    <th>{i18n._t('HistoryViewer.Author', 'Author')}</th>
-    {props.hasActions ? <th /> : null}
-  </tr>
-);
+class HistoryViewerHeading extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.handleCompareModeChange = this.handleCompareModeChange.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  handleCompareModeChange(event) {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+      this.props.onCompareModeSelect();
+    } else {
+      this.props.onCompareModeUnSelect();
+    }
+  }
+
+  render() {
+    const { compareModeSelected, hasActions } = this.props;
+    const { dropdownOpen } = this.state;
+
+    return (
+      <tr className="history-viewer__heading">
+        <th>#</th>
+        <th>{i18n._t('HistoryViewer.Record', 'Record')}</th>
+        <th className="author-compare-toggle__container">
+          <span className="author-span">{i18n._t('HistoryViewer.Author', 'Author')}</span>
+          <Dropdown
+            isOpen={dropdownOpen}
+            toggle={this.toggle}
+            className="compare-dropdown"
+            right
+          >
+            <DropdownToggle className="font-icon-sliders" />
+            <DropdownMenu>
+              <div className="form-check">
+                <input
+                  id="history-viewer-compare-two"
+                  type="checkbox"
+                  checked={compareModeSelected}
+                  onChange={this.handleCompareModeChange}
+                />
+                <label className="form-check-label" htmlFor="history-viewer-compare-two">
+                  {i18n._t('HistoryViewerHeading.COMPARE_VERSIONS', 'Compare 2 versions')}
+                </label>
+              </div>
+            </DropdownMenu>
+          </Dropdown>
+        </th>
+        {hasActions ? <th/> : null}
+      </tr>
+    );
+  }
+}
 
 HistoryViewerHeading.propTypes = {
-  hasActions: React.PropTypes.bool,
+  hasActions: PropTypes.bool,
+  compareModeSelected: PropTypes.bool,
 };
 
 HistoryViewerHeading.defaultProps = {
   hasActions: false,
 };
 
-export default HistoryViewerHeading;
+function mapStateToProps(state) {
+  return {
+    compareModeSelected: state.versionedAdmin.historyViewer.compareMode,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onCompareModeSelect() {
+      dispatch(setCompareMode(true));
+    },
+    onCompareModeUnSelect() {
+      dispatch(setCompareMode(false));
+    }
+  }
+}
+
+export { HistoryViewerHeading as Component };
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+)(HistoryViewerHeading);
