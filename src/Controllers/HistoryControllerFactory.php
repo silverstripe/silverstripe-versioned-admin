129ba12a09ca012adb44c9b98cba9ccf12f45205
Maxime Rainville <maxime@silverstripe.com>
2018-10-23 08:54:15 +1300

    API Deprecate HistoryControllerFactory and default to CMSPageHistoryViewerController

--- src/Controllers/HistoryControllerFactory.php
+++ src/Controllers/HistoryControllerFactory.php
@@ -13,6 +13,8 @@ use SilverStripe\Versioned\Versioned;
 /**
  * The history controller factory decides which CMS history controller to use, out of the default from the
  * silverstripe/cms module or the history viewer controller from this module, depending on the current page type
+ *
+ * @deprecated 1.0.0:2.0.0
  */
 class HistoryControllerFactory implements Factory
 {
@@ -21,31 +23,30 @@ class HistoryControllerFactory implements Factory
     public function create($service, array $params = array())
     {
         // If no request is available yet, return the default controller
-        if (!Injector::inst()->has(HTTPRequest::class)) {
-            return new CMSPageHistoryController();
-        }
-        $request = Injector::inst()->get(HTTPRequest::class);
-        $id = $request->param('ID');
+        if (Injector::inst()->has(HTTPRequest::class)) {
+            $request = Injector::inst()->get(HTTPRequest::class);
+            $id = $request->param('ID');
 
-        if ($id && is_numeric($id)) {
-            // Ensure we read from the draft stage at this position
-            $page = Versioned::get_one_by_stage(
-                SiteTree::class,
-                Versioned::DRAFT,
-                sprintf('"SiteTree"."ID" = \'%d\'', $id)
-            );
+            if ($id && is_numeric($id)) {
+                // Ensure we read from the draft stage at this position
+                $page = Versioned::get_one_by_stage(
+                    SiteTree::class,
+                    Versioned::DRAFT,
+                    sprintf('"SiteTree"."ID" = \'%d\'', $id)
+                );
 
-            if ($page && $this->isEnabled($page)) {
-                return Injector::inst()->create(CMSPageHistoryViewerController::class);
+                if ($page && !$this->isEnabled($page)) {
+                    return new CMSPageHistoryController();
+                }
             }
         }
 
         // Injector is not used to prevent an infinite loop
-        return new CMSPageHistoryController();
+        return Injector::inst()->create(CMSPageHistoryViewerController::class);
     }
 
     /**
-     * Only activate for pages that have a history viewer capability applied. Extensions can provide their
+     * Only deactivate for pages that have a history viewer capability removed. Extensions can provide their
      * own two cents about this criteria.
      *
      * @param SiteTree $record
@@ -54,6 +55,6 @@ class HistoryControllerFactory implements Factory
     public function isEnabled(SiteTree $record)
     {
         $enabledResults = array_filter($this->extend('updateIsEnabled', $record));
-        return (!empty($enabledResults) && max($enabledResults) === true);
+        return (empty($enabledResults) || min($enabledResults) === false);
     }
 }
