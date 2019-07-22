53c0efa1d7b8b479bfd71170b3f8042a610e314f
Robbie Averill <robbie@averill.co.nz>
2019-07-22 10:31:40 +0200

    DOCS Update deprecation tag syntax to use full semver version number

--- src/Extensions/CMSMainExtension.php
+++ src/Extensions/CMSMainExtension.php
@@ -5,7 +5,7 @@ namespace SilverStripe\VersionedAdmin\Extensions;
 use SilverStripe\Core\Extension;
 
 /**
- * @deprecated 1.2 This extension no longer serves a purpose. Write a standalone extensions instead.
+ * @deprecated 1.2.0 This extension no longer serves a purpose. Write a standalone extensions instead.
  */
 class CMSMainExtension extends Extension
 {
