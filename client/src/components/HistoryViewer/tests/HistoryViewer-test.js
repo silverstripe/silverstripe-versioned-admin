3dc3f6e29400e8439965c2c26c05eb7c9c4442b8
Guy Sartorelli <guy.sartorelli@gmail.com>
2025-05-13 17:46:27 +1200

    FIX Display an error when model history has errors

--- client/src/components/HistoryViewer/tests/HistoryViewer-test.js
+++ client/src/components/HistoryViewer/tests/HistoryViewer-test.js
@@ -200,3 +200,40 @@ test('HistoryViewer compoareModeAvailable() returns false with only one version'
   const el = await screen.findByTestId('test-version-detail');
   expect(el.getAttribute('data-comparemodeavailable')).toEqual('false');
 });
+
+test('HistoryViewer displays no error when there are none', async () => {
+  const error = jest.fn();
+  render(
+    <HistoryViewer {...makeProps({ toastActions: { error } })}/>
+  );
+  const el = await screen.queryByTestId('test-list');
+  expect(el).not.toBeFalsy();
+  expect(error.mock.calls.length).toBe(0);
+});
+
+test('HistoryViewer displays error when there is one on initial render', async () => {
+  const error = jest.fn();
+  render(
+    <HistoryViewer {...makeProps({ graphQLErrors: ['error 1', 'error 2'], toastActions: { error } })}/>
+  );
+  const el = await screen.queryByTestId('test-list');
+  expect(el).toBeFalsy();
+  expect(error.mock.calls.length).toBe(1);
+  expect(error.mock.calls[0][0]).toBe('An unknown error has occurred.');
+});
+
+test('HistoryViewer displays error when there is one in updated props', async () => {
+  const error = jest.fn();
+  const props = makeProps({ toastActions: { error } });
+  const { rerender, queryByTestId } = render(<HistoryViewer {...props}/>);
+  const el = await queryByTestId('test-list');
+  expect(el).not.toBeFalsy();
+  expect(error.mock.calls.length).toBe(0);
+
+  props.graphQLErrors = ['error 1', 'error 2'];
+  rerender(<HistoryViewer {...props}/>);
+  const el2 = await queryByTestId('test-list');
+  expect(el2).toBeFalsy();
+  expect(error.mock.calls.length).toBe(1);
+  expect(error.mock.calls[0][0]).toBe('An unknown error has occurred.');
+});
